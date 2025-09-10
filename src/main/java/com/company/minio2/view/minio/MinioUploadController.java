984a554e2879c79c37f463dcbf8582ba2d032e42
luudong <luuvandong2004@gmail.com>
2025-09-10 14:28:54 +0700

    api upload

---
+++ src/main/java/com/company/minio2/view/minio/MinioUploadController.java
@@ -0,0 +1,9 @@
+package com.company.minio2.view.minio;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/minio/upload")
+public class MinioUploadController {
+}
