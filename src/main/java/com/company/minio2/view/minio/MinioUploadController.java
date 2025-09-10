04ea95f17bb5b9dd6294757488030d64f50932dd
luudong <luuvandong2004@gmail.com>
2025-09-10 15:43:02 +0700

    api upload , delete file

--- src/main/java/com/company/minio2/view/minio/MinioUploadController.java
+++
@@ -1,9 +0,0 @@
-package com.company.minio2.view.minio;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/minio/upload")
-public class MinioUploadController {
-}
